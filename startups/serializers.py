8b469f75e64cccfbdef72b44814cc73456c5e57a
OllyRaret <olenka.lakeeva12@gmail.com>
2025-04-21 18:00:47 +0300

    Delete Old Startup Image on Updating

--- startups/serializers.py
+++ startups/serializers.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -60,7 +62,7 @@ class StartupSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False, allow_null=True)
     industry = IndustrySerializer(read_only=True)
     industry_id = serializers.PrimaryKeyRelatedField(
-        queryset=Industry.objects.all(), source='industry', write_only=True, required=False
+        queryset=Industry.objects.all(), source='industry', write_only=True, required=True
     )
     required_specialists = RequiredSpecialistSerializer(many=True) # ToDo
     is_favorited = serializers.SerializerMethodField()
@@ -72,16 +74,24 @@ class StartupSerializer(serializers.ModelSerializer):
             'stage', 'investment_needed', 'founder', 'required_specialists', 'is_favorited'
         ]
 
+
     def get_is_favorited(self, obj):
         # ToDo
         return False
 
 
+    def validate(self, attrs):
+        # Проверка на отрицательную инвестицию
+        investment = attrs.get('investment_needed')
+        if investment is not None and investment < 0:
+            raise ValidationError({'investment_needed': 'Сумма инвестиций не может быть отрицательной.'})
+
+        return attrs
+
+
     def create(self, validated_data):
         required_specialists_data = validated_data.pop('required_specialists', [])
         startup = Startup.objects.create(**validated_data)
-
-        print(required_specialists_data)
 
         for specialist_data in required_specialists_data:
             skills = specialist_data.pop('skills', [])
@@ -94,6 +104,18 @@ class StartupSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         required_specialists_data = validated_data.pop('required_specialists', None)
+
+        old_image = instance.image
+        new_image = validated_data.get('image', None)
+
+        # Удаление картинки, если передано null или другая картинка
+        if 'image' in validated_data and old_image and (
+            new_image is None or new_image != old_image
+        ):
+            image_path = old_image.path
+            instance.image.delete(save=False)
+            if os.path.isfile(image_path):
+                os.remove(image_path)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
