b4cd44e1b735fd9ba016816f0474cfe6c0388131
OllyRaret <olenka.lakeeva12@gmail.com>
2025-05-17 18:16:50 +0300

    Fix DEFAULT_FILTER_BACKENDS

--- backend/settings.py
+++ backend/settings.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
 
